bdcf5096f6583272f4aa8999bde68689cd109f4e
Ahmad Sattar <thehabbos007@gmail.com>
2018-10-05 11:58:10 +0200

    Changed the way dice are rolled

--- src/terning/Dice.java
+++ src/terning/Dice.java
@@ -3,16 +3,15 @@ package terning;
 public class Dice {
 	// roll the die and return the value (1-6)
 	public int roll() {
-		float d1=(float)Math.random();     // 0-1
-		float d2=d1*5;                     // 0-5
-		int d3=Math.round(d2);             // 0-5 integer
-		return d3 + 1;                     // 1-6		
+		// Instead of rounding floats, we cast the returning random number into
+		// integers, thus not relying on rounding, and getting a more equal spread
+		return (int)(Math.random()*6)+1;
 	}
-	
+
 	// roll the die n times and print the values
 	public void rollMultiple(int n) {
 		for (int i=0; i<n; i++) { //Fixed dice rolling 1 less than user input (1->0)
 			System.out.print(roll() + " ");
-		}		
+		}
 	}
 }
